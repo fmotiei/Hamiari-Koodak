84eb1b3cd09710712b137c8383cea83a416bcb4b
maryamelahi <maryam.elahiii@gmail.com>
2018-02-04 11:25:09 +0330

    tables added

--- karbar/admin.py
+++ karbar/admin.py
@@ -5,3 +5,10 @@ from .models import *
 admin.site.register(staff_members )
 
 admin.site.register(UserKarbar )
+admin.site.register(Payam)
+admin.site.register(Payam_Adi)
+admin.site.register(Payam_Madadju)
+admin.site.register(Payam_Madadju_Madadkar)
+admin.site.register(events)
+admin.site.register(kheirieh)
+admin.site.register(akhbar)
--- madadju/admin.py
+++ madadju/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Madadju)
+admin.site.register(sharhe_tahsil)
+admin.site.register(taghire_madadkar)
+admin.site.register(Niaz)
+admin.site.register(hadie_gheire_naghdi)
