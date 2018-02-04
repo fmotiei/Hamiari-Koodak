3f06aa6fff283c314710995e9ce7bc5e61cc8b78
maryamelahi <maryam.elahiii@gmail.com>
2018-02-04 11:28:57 +0330

    tables added

--- karbar/admin.py
+++ karbar/admin.py
@@ -6,6 +6,7 @@ admin.site.register(staff_members )
 
 admin.site.register(UserKarbar )
 # admin.site.register(Payam)
+
 admin.site.register(Payam_Adi)
 admin.site.register(Payam_Madadju)
 admin.site.register(Payam_Madadju_Madadkar)
